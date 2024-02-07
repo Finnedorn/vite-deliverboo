a829e5ab0a038aac276a44bfa838aaf954f75018
Finnedorn <daniele.giuntoli@outlook.com>
2024-02-07 12:33:54 +0100

    basic scaffholding

---
+++ src/App.vue
@@ -0,0 +1,26 @@
+<template>
+  <router-view :key="$route.path"></router-view>
+</template>
+
+<script>
+import axios from "axios";
+import { store } from "./data/store.js";
+export default {
+  name: 'App',
+  components: {
+  },
+  data() {
+    return {
+      store
+    };
+  },
+  methods: {
+  },
+  mounted() {
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+// @use "./assets/style/partials/variables" as *;
+</style>
---
+++ src/pages/AppHome.vue
@@ -0,0 +1,28 @@
+<template>
+    <h1>
+        paginina
+    </h1>
+</template>
+  
+<script>
+import axios from "axios";
+import { store } from "../data/store.js";
+export default {
+    name: 'AppHome',
+    components: {
+    },
+    data() {
+        return {
+            store
+        };
+    },
+    methods: {
+    },
+    mounted() {
+    }
+}
+</script>
+  
+<style lang="scss" scoped>
+// @use "./assets/style/partials/variables" as *;
+</style>
