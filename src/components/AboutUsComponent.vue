0f181b1278ec41969a3b6cb74cd5ec07a067272a
Finnedorn <daniele.giuntoli@outlook.com>
2024-02-16 17:58:37 +0100

    added about us component

---
+++ src/components/AboutUsComponent.vue
@@ -0,0 +1,30 @@
+<template>
+    <!-- About Us  -->
+    <div class="container-fluid green-wave-wrapper">
+        <img src="../assets/img/wave-green.png" alt="wave-green">
+    </div>
+    <div class="container-fluid about-us-wrapper pb-5">
+        <div class="text-center mb-5 pt-3">
+        </div>
+    </div>
+</template>
+  
+<script>
+export default {
+    name: "AboutUsComponent",
+};
+</script>
+  
+<style lang="scss" scoped>
+@use '../assets/style/partials/variables.scss' as *;
+
+.partners-wrapper {
+    background-color: $color-primary;
+    height: 300px;
+
+    .search-title {
+        font-size: 5.5rem;
+    }
+}
+
+</style>
