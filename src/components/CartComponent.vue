199ed0dd14fa85c599185a53c319d84617fe404e
Finnedorn <daniele.giuntoli@outlook.com>
2024-02-18 15:52:41 +0100

    added new style to cartcomponent

--- src/components/CartComponent.vue
+++ src/components/CartComponent.vue
@@ -1,31 +1,41 @@
 <template>
-    <div id="cart">
-        <!-- <div class="card">
-            <div class="card-head">carrello</div>
-            <div class="card-body">
-                <h2>hello</h2>
+    <div class=" card d-flex flex-column justify-content-between w-25 rounded-4 p-4">
+        <div id="cart">
+            <h3 class="text-center fs-2 mb-4 fw-bold">
+                Il tuo Ordine
+            </h3>
+            <div v-for="el in this.store.cart" :key="el.dish_id" class="d-flex justify-content-between align-items-center">
+                <div class="d-flex align-items-center">
+                    <button @click="removeDishCart(el.dish_id)" class="quantity-btn min mx-1">
+                        -
+                    </button>
+                    <div class="mx-1 ">
+                        {{ el.quantity }}
+                    </div>
+                    <button @click="addDishCart(el.dish_id)" class="quantity-btn plus mx-1">
+                        +
+                    </button>
+                    <div class="mx-1">{{ el.name }}</div>
+                </div>
+                <div>{{ el.price * el.quantity }} €</div>
             </div>
-        </div> -->
+        </div>
+        <div id="restaurantErrorMsg" class="d-none">Non puoi aggiungere un piatto di un altro ristorante</div>
         <div>
-            <table>
-                <thead>carrello</thead>
-                <th>prodotti</th>
-                <th>quantità</th>
-                <th>prezzo</th>
-                <tbody>
-                    <tr v-for="el in this.store.cart" :key="el.dish_id">
-                        <td>{{ el.name }}</td>
-                        <td>
-                            <button @click="removeDishCart(el.dish_id)">-</button>
-                            {{ el.quantity }}
-                        </td>
-                        <button @click="addDishCart(el.dish_id)">+</button>
-                        <td>{{ el.price * el.quantity }} €</td>
-                    </tr>
-                </tbody>
-            </table>
-            <div id="restaurantErrorMsg" class="d-none">Non puoi aggiungere un piatto di un altro ristorante</div>
-            <button @click="emptyCart()">Svuota</button>
+            <div class="border-top border-2 py-2">
+                Totale 
+                <span>{{ store.totalPrice }} €</span>
+            </div>
+            <div class="d-flex align-items-center justify-content-around my-2">
+                <button @click="emptyCart()" class="btn btn-empty  fw-bold me-3">
+                    Svuota <br> Carrello
+                </button>
+                <router-link :to="{ name: 'checkout' }">
+                    <button class="btn btn-checkout text-light fw-bold ">
+                        Vai al <br> Checkout
+                    </button>
+                </router-link>
+            </div>
         </div>
     </div>
 </template>
@@ -93,9 +103,46 @@ export default {
 @use '../assets/style/partials/variables.scss' as *;
 
 #cart {
-    // width: 100px;
-    padding: 20px;
+    min-height: 300px;
 
+    .quantity-btn {
+        color: $color-white;
+        background-color: $color-secondary;
+        border: none;
+        border-radius: 100px;
+        font-weight: bold;
+        font-size: 0.7rem;
+    }
+
+    .plus {
+        padding: 1px 5.5px;
+    }
+
+    .min {
+        padding: 1px 7px
+    }
 }
 
+
+.btn-checkout {
+    background-color: $color-primary;
+    border-radius: 5px;
+    padding: 5px 30px;
+
+    &:hover {
+        transform: scale(1.05);
+        transition: all 0.5s;
+    }
+}
+
+.btn-empty {
+    border: 5px solid $color-primary;
+    border-radius: 5px;
+    padding: 2px 30px;
+
+    &:hover {
+        transform: scale(1.05);
+        transition: all 0.5s;
+    }
+}
 </style>
