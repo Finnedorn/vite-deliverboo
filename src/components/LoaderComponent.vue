190a128b517bc696d9dd1b60133c1117f4109c13
Finnedorn <daniele.giuntoli@outlook.com>
2024-02-21 10:25:49 +0100

    created loader component

---
+++ src/components/LoaderComponent.vue
@@ -0,0 +1,20 @@
+<template>
+    <div class="container-fluid">
+        <div class="loader-wrapper d-flex justify-content-center align-items-center">
+            <img src="../assets/img/logo_food_red.png" alt="deliverboo-logo-loader">
+        </div>
+    </div>
+</template>
+  
+<script>
+export default {
+    name: "LoaderComponent",
+};
+</script>
+  
+<style lang="scss" scoped>
+@use '../assets/style/partials/variables.scss' as *;
+
+
+
+</style>
