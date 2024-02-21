fffd7a1eb75da0a30f4d4dc86e600c417e171da8
Finnedorn <daniele.giuntoli@outlook.com>
2024-02-21 17:37:32 +0100

    added new hero carousel component

---
+++ src/components/HeroCarouselComponent.vue
@@ -0,0 +1,15 @@
+<template>
+    
+</template>
+  
+<script>
+export default {
+    name: "HeroCarouselComponent",
+};
+</script>
+  
+<style lang="scss" scoped>
+@use '../assets/style/partials/variables.scss' as *;
+
+
+</style>
