747875a3d9f687959e485ea379ff98446f5bf1c5
Finnedorn <daniele.giuntoli@outlook.com>
2024-02-22 01:02:54 +0100

    partners carousel update

--- src/components/PartnersCarouselComponent.vue
+++ src/components/PartnersCarouselComponent.vue
@@ -1,9 +1,11 @@
 <template>
     <!-- Partners carousel  -->
     <!-- first -->
-    <Carousel :autoplay="4000" :wrap-around="true" :itemsToShow="1">
-        <slide id="slide" v-for="(food,index) in heroFoods" :key="index">
-            <img  :src=food.url :alt=food.name />
+    <Carousel :autoplay="1"  :wrap-around="true" :itemsToShow="12" :slidesCount="1" :transition="5000">
+        <slide  v-for="(food,index) in partnersFirst" :key="index">
+            <div class="overflow-hidden rounded-3 mx-1">
+                <img :src=food.url :alt=food.name />
+            </div>
         </slide>
     </Carousel>
     <!-- second -->
@@ -22,9 +24,33 @@ export default {
         return {
             partnersFirst: [
                 {
-                    name: 'wings',
-                    url: "/img/partner-wings.png",
-                }
+                    name: 'food',
+                    url: "/img/partner-ag",
+                },
+                {
+                    name: 'food',
+                    url: "/img/partner-kfc",
+                },
+                {
+                    name: 'food',
+                    url: "/img/partner-tortillas",
+                },
+                {
+                    name: 'food',
+                    url: "/img/partner-mc",
+                },
+                {
+                    name: 'food',
+                    url: "/img/partner-soba",
+                },
+                {
+                    name: 'food',
+                    url: "/img/partner-tikka",
+                },
+                {
+                    name: 'food',
+                    url: "/img/partner-bk",
+                },
             ]
         }
     }
